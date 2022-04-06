b0bfdbf2ff8816f9e105bef1ebe3bed2b70b583c
Ramon Palheta Pessoa <ramon.palheta.pessoa@hotmail.com>
2022-04-06 19:09:11 -0300

    Metodos em java

---
+++ java-metodos/src/br/comdigitalinnovationone/metodos/Quadrilatero.java
@@ -0,0 +1,11 @@
+package br.comdigitalinnovationone.metodos;
+
+public class Quadrilatero {
+    //Retorno
+    public static double area(double lado)
+    {
+        return lado * lado;
+
+
+    }
+}
