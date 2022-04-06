4e99250df000d0a5fab1ed99577081d1eb8322a9
Ramon Palheta Pessoa <ramon.palheta.pessoa@hotmail.com>
2022-04-06 18:21:02 -0300

    Aplicando testes com java

---
+++ testes_java/src/main/java/com/teste/Calculadora.java
@@ -0,0 +1,13 @@
+package com.teste;
+
+public class Calculadora {
+
+    public int somar(int n){
+        int soma = 0;
+
+            soma += n;
+
+        System.out.println(soma);
+        return soma;
+    }
+}
